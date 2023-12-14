b5ed58b1a0ee9f1a904a040c7e135c51025fbdd2
guxo2795 <guxo1211@gmail.com>
2023-12-14 18:23:30 +0900

    추가 기능 구현 : 수정, 삭제 시 에러 메시지 출력

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -2,14 +2,13 @@ package com.example.board.controller;
 
 import com.example.board.dto.BoardRequestDto;
 import com.example.board.dto.BoardResponseDto;
-import com.example.board.entity.Board;
 import com.example.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -33,13 +32,24 @@ public class BoardController {
     }
 
     @PutMapping("/boards/{id}")
-    public BoardResponseDto updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto) {
-        return boardService.updateBoard(id, requestDto.getPassword(), requestDto);
+    public ResponseEntity<?> updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto) {
+        try {
+            BoardResponseDto boardResponseDto = boardService.updateBoard(id, requestDto.getPassword(), requestDto);
+            return ResponseEntity.ok().body(boardResponseDto);
+        } catch(IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @DeleteMapping("/boards/{id}")
-    public Long deleteBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto) {
-        return boardService.deleteBoard(id, requestDto.getPassword());
+    public ResponseEntity<?> deleteBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto) {
+        try {
+            boardService.deleteBoard(id, requestDto.getPassword());
+            return new ResponseEntity<>("삭제 성공", HttpStatus.OK);
+        } catch(IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
     }
 
 }
